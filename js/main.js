c5462dba321c8b2f61d74ff4d93804c1604783bc
Jadou <ajadou@hotmail.com>
2014-02-18 17:09:53 +0300

    Markdown

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-document.onClick = function(){
-  console.log("Ahmed Jadou Did you click me?");
-}; 
+// document.onclick = function(){
+//   console.log("Ahmed Jadou Did you click me?");
+// }; 
+
+var images = document.getElementById("images");
+var imagesArray = ["./img/001.gif","./img/002.jpg","./img/003.png","./img/004.jpg"];
+var imageIndex = 0;
+
+function slideshow(){
+  images.setAttribute("src",imagesArray[imageIndex]);
+  imageIndex++;
+  if(imageIndex >= imagesArray.length){
+    imageIndex = 0;
+  }
+}
+  // set a timeout
+  //setTimeout(functionName,3000);
+
+  // repeat timer
+  var iH = setInterval(slideshow,5000);
+
+  document.onclick=function(){
+      console.log("youclick");
+      clearInterval(iH);
+    };
